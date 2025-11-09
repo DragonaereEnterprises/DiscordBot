8aeb6e5e16e4bd5126b2c932dd15d33a612b3f06
Andrew Still <astill@dragonaere.com>
2025-11-08 23:07:54 -0500

    Update logger.ts

--- logger.ts
+++ logger.ts
@@ -1,8 +1,8 @@
 import pino from 'pino';
 const transport = pino.transport({
   targets: [
-    { target: 'pino/file', options: { destination: '../logs/error.json' }, level: 'error' },
-    { target: 'pino/file', options: { destination: '../logs/bot.json' }, level: 'info'},
+    { target: 'pino/file', options: { destination: './logs/error.json' }, level: 'error' },
+    { target: 'pino/file', options: { destination: './logs/bot.json' }, level: 'info'},
     { target: 'pino-pretty', options: {destination: 1}, level:'info'}
   ],
 });
